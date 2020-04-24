cbf0675f2c47419f6b551ec5a1dfc2d55ee070a9
Dhruv Yadwadkar <dhruvyadwadkar@gmail.com>
2020-04-23 23:43:46 -0400

    [ADD] Custom Middleware

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,12 +3,14 @@ const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const gravatar = require('gravatar');
 const User = require('../../models/User')
+const jwt = require('jsonwebtoken')
 const brcrypt = require('bcrypt')
+const config = require('config')
 
 // @router      POST api/users
 // @desc        Register User
 // @access      Public
-router.get('/'
+router.post('/api/users'
     , [check('name', 'Name is required!').not().isEmpty(),
     check('email', 'Include a valid email').isEmail(),
     check('password', 'Enter a password with 5 or more characters').isPassword().isLength({ min: 5 })]
@@ -47,13 +49,22 @@ router.get('/'
             user.password = await brcrypt.hash(password, salt);
             await user.save()
 
-
             // Return JWT
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            }
+
+            jwt.sign(payload, config.get('jwtSecret'), { expiresIn: 360000 }, (err, token) => {
+                if (err) throw err
+                res.json({ token })
+            })
+
         } catch (error) {
             console.error(error.message);
             res.status(500).send('Server Error!');
         }
-
     });
 
 module.exports = router;
