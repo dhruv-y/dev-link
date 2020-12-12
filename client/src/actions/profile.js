64481b4634a4b5ace08a4484238ffb1f505a09e4
Dhruv Yadwadkar <dhruvyadwadkar@gmail.com>
2020-12-12 10:27:48 +0530

    Get all profiles + profile by ID added (redux)

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -3,8 +3,11 @@ import { setAlert } from './alert';
 
 import {
     GET_PROFILE,
+    GET_PROFILES,
     PROFILE_ERROR,
-    UPDATE_PROFILE
+    UPDATE_PROFILE,
+    CLEAR_PROFILE,
+    GET_REPOS
 } from './types';
 
 // Current user profile
@@ -13,6 +16,56 @@ export const getCurrentProfile = () => async dispatch => {
         const res = await axios.get('/api/profile/me');
         dispatch({
             type: GET_PROFILE,
+            payload: res.data
+        })
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        });
+    }
+};
+
+// Get all user profiles
+export const getProfiles = () => async dispatch => {
+    dispatch({ type: CLEAR_PROFILE })
+
+    try {
+        const res = await axios.get('/api/profile');
+        dispatch({
+            type: GET_PROFILES,
+            payload: res.data
+        })
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        });
+    }
+};
+
+// Get profile by ID
+export const getProfileById = userId => async dispatch => {
+    try {
+        const res = await axios.get(`/api/profile/user/${userId}`);
+        dispatch({
+            type: GET_PROFILE,
+            payload: res.data
+        })
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        });
+    }
+};
+
+// Get github repos
+export const getGithubRepos = username => async dispatch => {
+    try {
+        const res = await axios.get(`/api/profile/github/${username}`);
+        dispatch({
+            type: GET_REPOS,
             payload: res.data
         })
     } catch (err) {
--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -2,7 +2,9 @@ import {
     GET_PROFILE,
     PROFILE_ERROR,
     UPDATE_PROFILE,
-    CLEAR_PROFILE
+    CLEAR_PROFILE,
+    GET_PROFILES,
+    GET_REPOS
 } from '../actions/types';
 
 const initialState = {
@@ -24,6 +26,13 @@ export default function (state = initialState, action) {
                 loading: false
             }
 
+        case GET_PROFILES:
+            return {
+                ...state,
+                profiles: payload,
+                loading: false
+            }
+
         case PROFILE_ERROR:
             return {
                 ...state,
@@ -37,6 +46,13 @@ export default function (state = initialState, action) {
                 profile: null,
                 repos: [],
                 loading: false
+            }
+
+        case GET_REPOS:
+            return {
+                ...state,
+                repos: payload,
+                loading: false   
             }
 
         default:
